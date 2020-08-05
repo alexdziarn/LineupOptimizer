e52068afceb070449eeddf1df6a7248113b5f263
alex-windows-pc <alexdziarn@gmail.com>
2020-08-05 19:57:48 -0400

    added variables to the optimize function

--- run.py
+++ run.py
@@ -2,50 +2,53 @@ import pandas as pd
 import pulp
 
 print("running optimization")
-# create datatable
-da = pd.read_excel('DFS.xls')
-df = pd.DataFrame(da, columns=["Name", "Price", "Value", "Pos"])
 
-# create Problem
-prob = pulp.LpProblem("ExpectedScore", pulp.LpMaximize)
+def optimize(excelsheet, namecol, pricecol, valuecol, poscol):
 
-# create list of players
-players = list(df["Name"])
+    # create datatable
+    da = pd.read_excel(excelsheet)
+    df = pd.DataFrame(da, columns=[namecol, pricecol, valuecol, poscol])
 
-# create dict of prices
-prices = dict(zip(players, df["Price"]))
+    # create list of players
+    players = list(df[namecol])
 
-# create dict of values/expected fantasy points
-values = dict(zip(players, df["Value"]))
+    # create dict of prices
+    prices = dict(zip(players, df[pricecol]))
 
-# create dict of posistions
-positions = dict(zip(players, df["Pos"]))
+    # create dict of values/expected fantasy points
+    values = dict(zip(players, df[valuecol]))
 
-# create variables
-inout = pulp.LpVariable.dicts("inout", players, lowBound=0, cat="Binary")
+    # create dict of posistions
+    positions = dict(zip(players, df[poscol]))
+    
+    # create Problem
+    prob = pulp.LpProblem("ExpectedScore", pulp.LpMaximize)
 
-# objective function
-prob += pulp.lpSum([inout[x] * values[x] for x in players])
+    # create variables
+    inout = pulp.LpVariable.dicts("inout", players, lowBound=0, cat="Binary")
 
-# constraints
-salary = 60000
-prob += pulp.lpSum([inout[x] * prices[x] for x in players]) <= salary
-prob += pulp.lpSum([inout[x] for x in players if positions[x] == "PG"]) == 2
-prob += pulp.lpSum([inout[x] for x in players if positions[x] == "SG"]) == 2
-prob += pulp.lpSum([inout[x] for x in players if positions[x] == "SF"]) == 2
-prob += pulp.lpSum([inout[x] for x in players if positions[x] == "PF"]) == 2
-prob += pulp.lpSum([inout[x] for x in players if positions[x] == "C"]) == 1
+    # objective function
+    prob += pulp.lpSum([inout[x] * values[x] for x in players])
 
-# solve
-prob.solve()
+    # constraints
+    salary = 60000
+    prob += pulp.lpSum([inout[x] * prices[x] for x in players]) <= salary
+    prob += pulp.lpSum([inout[x] for x in players if positions[x] == "PG"]) == 2
+    prob += pulp.lpSum([inout[x] for x in players if positions[x] == "SG"]) == 2
+    prob += pulp.lpSum([inout[x] for x in players if positions[x] == "SF"]) == 2
+    prob += pulp.lpSum([inout[x] for x in players if positions[x] == "PF"]) == 2
+    prob += pulp.lpSum([inout[x] for x in players if positions[x] == "C"]) == 1
 
-# print the status
-print(pulp.LpStatus[prob.status])
+    # solve
+    prob.solve()
 
-# print the optimal lineup
-for z in prob.variables():
-    if z.varValue > 0:
-        print(z.name[6:] + " = " + str(z.varValue))
 
-# print expected fantasy point total
-print("Expected Score: " + str(pulp.value(prob.objective)))
+    # print the optimal lineup
+    for z in prob.variables():
+        if z.varValue > 0:
+            print(z.name[6:])
+
+    # print expected fantasy point total
+    print("Expected Score: " + str(pulp.value(prob.objective)))
+
+optimize("DFS.xls", "Name", "Price", "Value", "Pos")
